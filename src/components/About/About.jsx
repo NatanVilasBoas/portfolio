47fa7087fdb48952d9530709f09817b43593ca1d
Natan200-2 <natanboas21@outlook.com>
2024-01-31 17:18:35 -0300

    curriculo aplicado

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -56,7 +56,9 @@ const About = () => {
                     <h3 style={{ fontSize: '40px' }}>Sobre mim</h3>
                     <p style={{ fontFamily: 'poppins', fontWeight: '300' }}>Sou programador e designer, tenho muito apreço pelas áreas por ter a chance de criar coisas que realmente venham ser úteis e que auxiliem meus clientes, criando projetos com um olhar mais detalhista envolto não apenas na usabilidade do projeto, mas na experiência do usuário e no conforto visual também</p>
                 </Text>
-                <Button text="currículo" color="#301E67" colorLight="#6a5bcd" padding="12px 32px"/>
+                <a href="./Curriculo.pdf" target="_blank">
+                    <Button text="currículo" color="#301E67" colorLight="#6a5bcd" padding="12px 32px"/>
+                </a>
             </Content>
         </Container>
     )
